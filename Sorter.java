5e494a32d1742b0fb1456787366e6ffa82737351
zezo3sl <zied.200232@gmail.com>
2023-02-14 21:16:13 +0300

    ea rb

--- Sorter.java
+++ Sorter.java
@@ -23,5 +23,20 @@ public class Sorter {
             arr[index] = arr[i];  
             arr[i] = smallerNumber;  
         }  
-    }  
+    }
+    public void insert(int a[], int n) /* function to sort an aay with insertion sort */
+    {
+        int i, j, temp;
+        for (i = 1; i < n; i++) {
+            temp = a[i];
+            j = i - 1;
+
+            while(j>=0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/
+            {
+                a[j+1] = a[j];
+                j = j-1;
+            }
+            a[j+1] = temp;
+        }
+    }
 }
